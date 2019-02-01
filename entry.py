68b40c099e3fd53efb52a464f85ff09406776247
Ethan Peterson <ethan@petetech.net>
2019-02-01 11:59:36 -0500

    added array field processing

--- entry.py
+++ entry.py
@@ -9,6 +9,17 @@ def  fieldPrompt(prompt, dbField):
     b[str(dbField)] = ans
     print("")
 
+def arrayField(prompt, dbField):
+    arr = []
+    print("ARRAY FIELD (GIVE COMMA SEPERATED LIST)")
+    ans = input(prompt)
+    ans = ans.split(",")
+    for i in range(0, len(ans)):
+        ans[i] = ans[i].strip()
+        arr.append(ans[i])
+    b[str(dbField)] = arr
+    print("")
+
 def newBuilding():
     print("Welcome to Building Entry Wizard")
     print("Press enter to move onto the next property (all fields mandatory)\n")
@@ -18,7 +29,7 @@ def newBuilding():
     fieldPrompt("faculty: ", "faculty")
     fieldPrompt("hours: ", "hours")
     fieldPrompt("history: ", "history")
-    fieldPrompt("alias (give comma seperated list of strings no spaces between commas): ", "alias")
+    arrayField("alias: ", "alias")
     fieldPrompt("services (give comma seperated list of strings no spaces between commas): ", "services")
 
     print("Final Document:\n")
