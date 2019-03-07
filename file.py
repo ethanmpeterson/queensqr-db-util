139df132622eb67bd955872ee059f85b828d0ea0
Ethan Peterson <ethan@petetech.net>
2019-03-07 18:13:57 -0500

    started on file selection of floorplan images #2

---
+++ file.py
@@ -0,0 +1,7 @@
+import easygui
+
+#path = easygui.fileopenbox()
+
+def selector(fileType):
+    return easygui.fileopenbox(msg = "Select A PNG File", title = "Specify PNG", filetypes = fileType) # returns string of file path to be used in upload process
+
