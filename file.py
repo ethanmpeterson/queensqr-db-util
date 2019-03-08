5782d0399546e2be6c375f01c6b7356912c50b10
Ethan Peterson <17emp5@queensu.ca>
2019-03-08 14:15:12 -0500

    file opener working

--- file.py
+++ file.py
@@ -5,3 +5,4 @@ import easygui
 def selector(fileType):
     return easygui.fileopenbox(msg = "Select A PNG File", title = "Specify PNG", filetypes = fileType) # returns string of file path to be used in upload process
 
+selector("*.png")
