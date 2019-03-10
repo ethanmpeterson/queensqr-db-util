f733d4370252651ec6d396818c6bf708e30533d8
Ethan Peterson <17emp5@queensu.ca>
2019-03-10 19:38:31 -0400

    got single file upload running with requests library still got 404 error due to incorrect api endpoint from the json

--- web.py
+++ web.py
@@ -6,6 +6,11 @@ properties = {}
 def uploadFile(paths): # Takes a string array of file Path and Uploads them with progress bar
     with open("properties.json") as props:
         properties = json.load(props)
-    print(properties)
-    print(paths)
+    url = properties['url']
+    print(url)
+    files = {'file' : ('0.png', open(paths[0], 'rb'))}
+    # for i, fPath in enumerate(paths):
+    #     files.append({'file' : (str(i) + '.png', open(fPath, 'rb'))})
+    r = requests.post(url, files = files)
+    print(r.status_code)
     pass
