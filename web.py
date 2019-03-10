c6fe0d7ed99dcf0d490f37a6c3e44c21fbb8a941
Ethan Peterson <17emp5@queensu.ca>
2019-03-09 20:20:46 -0500

    added json properties file to store api root #2

--- web.py
+++ web.py
@@ -1,4 +1,10 @@
 import requests
+import json
+
+properties = {}
 
 def uploadFile(paths): # Takes a string array of file Path and Uploads them with progress bar
+    with open("properties.json") as props:
+        properties = json.load(props)
+    print(properties)
     pass
